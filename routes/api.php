6f21b293184f08542573eed4dae808f1547787ee
aagarcia25 <109979605+aagarcia25@users.noreply.github.com>
2023-12-13 10:44:06 -0600

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -24,7 +24,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::group([
 'prefix' => 'SINEIN',
- 'middleware' => [ThrottleRequests::class.':10,1'], // 2 intentos en 1 minuto
 ], function () {
     Route::post('login', [LoginController::class, 'login']);
 });
